fac92da86a80a540f00c491686b573859131cd57
Dan Sapala <dsapala@gmail.com>
2013-05-14 21:35:43 -0400

    escaping double quote

--- src/pghstore_clj/core.clj
+++ src/pghstore_clj/core.clj
@@ -8,16 +8,24 @@
 (defprotocol FHstorable
   (from-hstore [this]))
 
+(defn- escape-sql-string [s]
+  (st/escape (str s) {\" "\\\""}))
+
+(defn- create-key-value-pair [k v]
+  (format "\"%s\"=>\"%s\"" (name k) (escape-sql-string v)))
+
+(defn- get-string-between-quotes [s]
+  (subs s 1 (- (.length s) 1)))
+
 (extend-type clojure.lang.IPersistentMap
   THstorable
   (to-hstore [this]
     (doto (PGobject.)
       (.setType "hstore")
       (.setValue
-       (apply str
-              (interpose ", "
-                         (for [[k v] this]
-                           (format "\"%s\"=>\"%s\"" (name k) v))))))))
+        (apply str
+               (interpose ", "
+                          (for [[k v] this] (create-key-value-pair k v))))))))
 
 (extend-type org.postgresql.util.PGobject
   FHstorable
@@ -26,6 +34,7 @@
       (into {}
             (for [[k v]
                   (map (fn [v]
-                         (map #(st/replace % #"\"" "") (st/split v #"=>")))
+                         (map get-string-between-quotes (st/split v #"=>")))
                        (st/split (.getValue this) #", "))]
               [(keyword k) v])))))
+
--- test/pghstore_clj/core_test.clj
+++ test/pghstore_clj/core_test.clj
@@ -15,4 +15,10 @@
       (is (= {:name "test" :username "root"} (from-hstore pgo))))
     (testing "doesn't create a hash if the type isn't hstore"
       (is (= nil (from-hstore (doto pgo (.setType "blah"))))))))
-    
+
+(deftest test-escaping-double-quote
+  (let [pgo (to-hstore {:name "te\"st" :username "ro\"ot"})]
+    (println (str pgo))
+    (testing "saving a string with a double quote in it"
+      (is (= {:name "te\\\"st" :username "ro\\\"ot"} (from-hstore pgo))))))
+
